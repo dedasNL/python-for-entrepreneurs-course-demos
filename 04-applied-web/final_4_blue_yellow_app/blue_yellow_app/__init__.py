51e2c6ca2ae807a0e822774bf72a16fa5a9698a3
Manfred Dasselaar <manfred.dasselaar@gmail.com>
2020-05-19 16:19:33 +0200

    Formatting PEP8

--- 04-applied-web/final_4_blue_yellow_app/blue_yellow_app/__init__.py
+++ 04-applied-web/final_4_blue_yellow_app/blue_yellow_app/__init__.py
@@ -16,7 +16,8 @@ def main(_, **settings):
 def init_routing(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
 
-    config.add_handler('root', '/', handler=home.HomeController, action='index')
+    config.add_handler(
+        'root', '/', handler=home.HomeController, action='index')
 
     add_controller_routes(config, home.HomeController, 'home')
     add_controller_routes(config, albums.AlbumsController, 'albums')
@@ -26,11 +27,16 @@ def init_routing(config):
 
 
 def add_controller_routes(config, ctrl, prefix):
-    config.add_handler(prefix + 'ctrl_index', '/' + prefix, handler=ctrl, action='index')
-    config.add_handler(prefix + 'ctrl_index/', '/' + prefix + '/', handler=ctrl, action='index')
-    config.add_handler(prefix + 'ctrl', '/' + prefix + '/{action}', handler=ctrl)
-    config.add_handler(prefix + 'ctrl/', '/' + prefix + '/{action}/', handler=ctrl)
-    config.add_handler(prefix + 'ctrl_id', '/' + prefix + '/{action}/{id}', handler=ctrl)
+    config.add_handler(prefix + 'ctrl_index', '/' + prefix,
+                       handler=ctrl, action='index')
+    config.add_handler(prefix + 'ctrl_index/', '/' + prefix + '/',
+                       handler=ctrl, action='index')
+    config.add_handler(prefix + 'ctrl', '/' + prefix +
+                       '/{action}', handler=ctrl)
+    config.add_handler(prefix + 'ctrl/', '/' + prefix +
+                       '/{action}/', handler=ctrl)
+    config.add_handler(prefix + 'ctrl_id', '/' + prefix +
+                       '/{action}/{id}', handler=ctrl)
 
 
 def init_includes(config):
